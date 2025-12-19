d859eaf49073c4dfbda7cf522c0d2ca0001a5448
THETITAN220 <raunakrose15@gmail.com>
2025-12-19 12:59:45 +0530

    FIX: Treesitter unable to attach to buffer

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,7 +3,7 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-        flavour = "mocha",     -- latte | frappe | macchiato | mocha
+        flavour = "mocha", -- latte | frappe | macchiato | mocha
         transparent_background = false,
 
         integrations = {
